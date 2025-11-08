03eb36d3cf73ce586ee35a98b5777a9f3322bd30
Ben Johnson <157973223+benjohnsn@users.noreply.github.com>
2025-11-08 20:37:42 +0000

    created all the files needed and moved some main code to gui

---
+++ gui.py
@@ -0,0 +1,6 @@
+import pygame as py
+
+width = height = 800
+dimension = 8
+sqSize = width // dimension
+fps  = 15
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import pygame as py
 
-width = height = 800
-dimension = 8
-sqSize = width // dimension
-fps  = 15
-
 def main():
     py.init()
     screen = py.display.set_mode((width, height))

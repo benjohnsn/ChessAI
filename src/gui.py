0bfc95cc7b40706083ad8cc6a4d4efe95533e6ff
Ben Johnson <157973223+benjohnsn@users.noreply.github.com>
2025-11-09 13:40:23 +0000

    updated constants, gui is able to display pieces

--- src/gui.py
+++ src/gui.py
@@ -1,25 +1,24 @@
 import pygame
-from constants import HEIGHT, WIDTH, DIMENSION, SQ_SIZE, COL1, COL2
+from constants import SIZE, CAPTION, DIMENSION, SQ_SIZE, COL1, COL2
 
 class Gui:
     def __init__(self):
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("Chess")
+        self.screen = pygame.display.set_mode(SIZE)
+        pygame.display.set_caption(CAPTION)
         self.images = {}
         self.loadimages()
 
     def loadimages(self):
         pieces = ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bP', 'bR', 'bN', 'bB', 'bQ', 'bK']
         for piece in pieces:
-            self.images[piece] = pygame.image.load("images/" + piece  + ".png"), (SQ_SIZE, SQ_SIZE)
+            self.images[piece] = pygame.transform.scale(pygame.image.load("images/" + piece  + ".png"), (SQ_SIZE, SQ_SIZE))
     
     def draw(self, board):
-        self.colours = [pygame.Color(COL1), pygame.Color(COL2)]
+        colours = [pygame.Color(COL1), pygame.Color(COL2)]
         for row in range(DIMENSION):
             for col in range(DIMENSION):
                 rect = pygame.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE)
-                pygame.draw.rect(self.screen, self.colours[(row + col) % 2], rect)
+                pygame.draw.rect(self.screen, colours[(row + col) % 2], rect)
                 piece = board.grid[row][col]
                 if piece != None:
-                    rect = pygame.Rect(col *SQ_SIZE, row *SQ_SIZE, SQ_SIZE, SQ_SIZE)
-                    self.screen(self.images[piece], rect)
+                    self.screen.blit(self.images[piece.colour + piece.type], rect)

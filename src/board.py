b3a7ea4e9c252b52b6b4caae5ddd0502be6f989a
Ben Johnson <157973223+benjohnsn@users.noreply.github.com>
2025-11-15 22:17:01 +0000

    Refactored generatepawnmoves logic, added comments to function in board

--- src/board.py
+++ src/board.py
@@ -12,26 +12,28 @@ class Board:
             [Piece('b','P') for _ in range(8)],
             [Piece('b','R'), Piece('b','N'), Piece('b','B'), Piece('b','Q'), Piece('b','K'), Piece('b','B'), Piece('b','N'), Piece('b','R')]
         ]       
-
         
     def getPiece(self, square):
+        # Given a square, it will return the designated piece
         row, col = square
         return self.grid[row][col]
     
     def makeMove(self, pieceSq, targetSq):
+        # Moves a piece to the target square
         startRow, startCol = pieceSq
         endRow, endCol = targetSq
         self.grid[endRow][endCol] = self.grid[startRow][startCol]
         self.grid[startRow][startCol] = None
 
     def generateLegalMoves(self, piece, square):
+        # Returns the generated list of moves dependant on the piece
         match piece.type:
             case 'P': return self.generatePawnMoves(piece, square)
-            case 'N': return self.generatePawnMoves(piece, square)
-            case 'B': return self.generatePawnMoves(piece, square)
-            case 'R': return self.generatePawnMoves(piece, square)
-            case 'Q': return self.generatePawnMoves(piece, square)
-            case 'K': return self.generatePawnMoves(piece, square)
+            case 'N': return self.generateKnightMoves(piece, square)
+            case 'B': return self.generateBishopMoves(piece, square)
+            case 'R': return self.generateRookMoves(piece, square)
+            case 'Q': return self.generateQueenMoves(piece, square)
+            case 'K': return self.generateKingMoves(piece, square)
 
     def generatePawnMoves(self, piece, square):
         row, col = square
@@ -53,16 +55,12 @@ class Board:
                 if target is None:
                     moves.append((row + direction * 2, col))
 
-        if self.inBounds(row + direction, col - 1):
-            target = self.grid[row + direction][col - 1]
-            if target and target.colour != piece.colour:
-                moves.append((row + direction, col - 1))
-
-        if self.inBounds(row + direction, col + 1):
-            target = self.grid[row + direction][col + 1]
-            if target and target.colour != piece.colour:
-                moves.append((row + direction, col + 1))
-
+        for offset in (-1, 1):
+            if self.inBounds(row + direction, col + offset):
+                target = self.grid[row + direction][col + offset]
+                if target and target.colour != piece.colour:
+                    moves.append((row + direction, col + offset))
+                    
         return moves
 
     def generateKnightMoves(self, piece, square):
@@ -77,4 +75,5 @@ class Board:
         pass
 
     def inBounds(self, row, col):
+        # Checks if square is on the board
         return (0 <= row < 8) and (0 <= col < 8)
